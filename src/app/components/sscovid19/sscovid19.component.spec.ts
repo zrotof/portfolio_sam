99bc3a400402193885d2000c2aec6d6e17834e66
Zrotof <manduel21@gmail.com>
2021-09-16 03:09:29 +0200

    chagin angular version 10 to 11

--- src/app/components/sscovid19/sscovid19.component.spec.ts
+++ src/app/components/sscovid19/sscovid19.component.spec.ts
@@ -1,4 +1,4 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { ComponentFixture, TestBed, waitForAsync } from '@angular/core/testing';
 
 import { Sscovid19Component } from './sscovid19.component';
 
@@ -6,7 +6,7 @@ describe('Sscovid19Component', () => {
   let component: Sscovid19Component;
   let fixture: ComponentFixture<Sscovid19Component>;
 
-  beforeEach(async(() => {
+  beforeEach(waitForAsync(() => {
     TestBed.configureTestingModule({
       declarations: [ Sscovid19Component ]
     })
