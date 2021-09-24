1973e6d1d11f3d9a2cc2f6ff4651684739ee42e9
Zrotof <manduel21@gmail.com>
2021-09-24 03:06:36 +0200

    correcting some express errors

--- server.js
+++ server.js
@@ -1,17 +1,16 @@
-function requireHTTPS(req, res, next) {
-    // The 'x-forwarded-proto' check is for Heroku
-    if (!req.secure && req.get('x-forwarded-proto') !== 'https') {
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    next();
-}
 const express = require('express');
-const app = express();
-app.use(requireHTTPS);
+const path = require('path');
 
-app.get('/*', function(req, res) {
-    res.sendFile('index.html', {root: 'dist/portfolio/'}
-  );
-  });
+const app = express();
+
+// Serve only the static files form the dist directory
+app.use(express.static(__dirname + '/dist/portfolio'));
+
+app.get('/*', function(req,res) {
+    
+res.sendFile(path.join(__dirname+'/dist/portfolio/index.html'));
+});
+
+
 // Start the app by listening on the default Heroku port
 app.listen(process.env.PORT || 8080);
