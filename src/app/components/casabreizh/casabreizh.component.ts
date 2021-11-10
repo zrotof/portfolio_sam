1bf58a917fd346ecdcfb32a02bb49090b915f853
kevin2993 <kevin.nankia@gmail.com>
2021-11-10 22:04:10 +0100

    adding responsive image on deviceson projects

--- src/app/components/casabreizh/casabreizh.component.ts
+++ src/app/components/casabreizh/casabreizh.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router} from '@angular/router';
+import { Router } from '@angular/router';
 import { faBackward } from '@fortawesome/free-solid-svg-icons';
 
 
@@ -11,18 +11,20 @@ import { faBackward } from '@fortawesome/free-solid-svg-icons';
 export class CasabreizhComponent implements OnInit {
 
   constructor( private router: Router ) { }
-  posterLink = "../../../assets/img/casabreizh.png";
+  posterLink = "../../../assets/img/casabreizh/casabreizh.png";
 
   faBackward = faBackward;
 
   ngOnInit(): void {
   }
+
   goOnProjectsList(){
     this.router.navigate(['réalisations']);
   }
 
   goOnElitesVoyagesWebsite(){
 
+    window.open("https://casabreizh.herokuapp.com", "_blank");
   }
 
 }
