2aa11d59d84570e9d5ace43bb25af21eaed7ae72
zrotof <manduel21@gmail.com>
2021-12-03 03:51:52 +0100

    styling

--- src/app/components/about/about.component.ts
+++ src/app/components/about/about.component.ts
@@ -28,9 +28,47 @@ export class AboutComponent implements OnInit {
   
   posterLink = "../../../assets/img/sscovid19.png";
 
+  currentContent:number  = 0;
+
+
   constructor() { }
 
   ngOnInit(): void {
+
+    this.activeContent(this.currentContent);
   }
 
+
+  //The fucntion will set the clicked header and set the right content to display
+  //We remove the active class on headers and set it to the clicked one
+  //We disable display from content and only displaying the good one
+  activeContent(param:number){
+
+
+    if(param != this.currentContent){
+
+      this.currentContent = param;
+
+    const headers = <NodeListOf<HTMLElement>>document.querySelectorAll(".profil-header");
+    const contents = <NodeListOf<HTMLElement>>document.querySelectorAll(".profil-info");
+
+    headers.forEach(element => {
+      element.classList.remove("active-header");
+    });
+
+    contents.forEach(element =>{
+      element.style.display="none";
+    })
+
+
+    headers[param].classList.add("active-header")
+
+
+    contents[param].style.display="flex";
+  
+  }
+
+
+
+  }
 }
